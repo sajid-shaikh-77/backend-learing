740b2b08da62f7df91e2e0cc5eae4b8a284d2841
sajid-shaikh-77 <mrsid966@gmail.com>
2026-04-05 18:14:33 +0530

    controller and route introduction

--- src/app.js
+++ src/app.js
@@ -14,6 +14,15 @@ app.use(express.urlencoded({
     limit:'16kb'
 }))
 app.use(express.static("public"))
-app.use(cookieParser )
+app.use(cookieParser())
+
+
+// routes import 
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+// app.use("/users",userRouter)
+app.use("api/v1/users",userRouter)
 
 export { app }
