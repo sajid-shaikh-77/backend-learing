137710ac646b2c704e41d49db596f6d7cb485330
sajid-shaikh-77 <mrsid966@gmail.com>
2026-04-08 19:21:24 +0530

    fixed register APi removed logs Test register API

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)\
-        console.log(file, "file");
+        // console.log(file, "file");
 
         // cb(null, file.fieldname)
         cb(null, file.originalname )
