d478ac141d07386e6bfdb87f80a7db6acf03adad
sajid-shaikh-77 <mrsid966@gmail.com>
2026-05-09 20:54:11 +0530

    fix: replace refreshToken update with unset operation in logoutUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -159,8 +159,11 @@ const logoutUser = asyncHandler(async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            // $set: {
+            //     refreshToken: undefined
+            // },
+            $unset: {
+                refreshToken: 1  // this remove the field from document
             }
         },
         {
